28669b13e372a52677caae8c983298bad07bf8b6
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-11-14 12:27:12 -0600

    Moves test phrases to the Spec Helper to keep the test file clean.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 require_relative '../some_class'
 
+FIRST_LINE = '99 bottles of beer on the wall. 99 bottles of beer. Take one down, pass it around, 98 bottles of beer on the wall.'
+
+LAST_LINE = 'No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.'
+
+TWO_BOTTLES_LEFT = '2 bottles of beer on the wall. 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall.'
+
+ONE_BOTTLE_LEFT = '1 bottle of beer on the wall. 1 bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.'
