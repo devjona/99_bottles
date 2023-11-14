ad603f3a54374e300026aa8c3fb5ee595e474b41
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-11-14 15:26:00 -0600

    frozen_string_literal at top, .freeze not needed anymore.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require_relative '../some_class'
 
-FIRST_LINE = '99 bottles of beer on the wall. 99 bottles of beer. Take one down, pass it around, 98 bottles of beer on the wall.'.freeze
+FIRST_LINE = '99 bottles of beer on the wall. 99 bottles of beer. Take one down, pass it around, 98 bottles of beer on the wall.'
 
-LAST_LINE = 'No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.'.freeze
+LAST_LINE = 'No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.'
 
-TWO_BOTTLES_LEFT = '2 bottles of beer on the wall. 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall.'.freeze
+TWO_BOTTLES_LEFT = '2 bottles of beer on the wall. 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall.'
 
-ONE_BOTTLE_LEFT = '1 bottle of beer on the wall. 1 bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.'.freeze
+ONE_BOTTLE_LEFT = '1 bottle of beer on the wall. 1 bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.'
