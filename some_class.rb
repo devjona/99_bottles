9f97ad4757305895d6241cf59690322cf4b1f4e4
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-11-14 12:22:46 -0600

    Adds more test cases Refactor, need to account for when 'bottle' should be plural or singular

--- some_class.rb
+++ some_class.rb
@@ -8,19 +8,9 @@ class BottlesSongMaker
 
   @@last_line = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, #{@@initial_quantity} bottles of beer on the wall."
 
-  def song_line_builder(quantity)
-    last_use = case quantity
-               when quantity == 1
-                 'no more'
-               else quantity - 1
-               end
-
-    "#{quantity} bottles of beer on the wall. #{quantity} bottles of beer. Take one down, pass it around, #{last_use} bottles of beer on the wall."
-  end
-
   def print_lyrics_to_array
     while @current_quantity >= 0
-      if @current_quantity > 0
+      if @current_quantity.positive?
         @array_of_lyrics.push(song_line_builder(@current_quantity))
       else
         @array_of_lyrics.push(@@last_line)
@@ -29,5 +19,28 @@ class BottlesSongMaker
       @current_quantity -= 1
     end
     @array_of_lyrics
+  end
+
+  private
+
+  def bottle_phrase_builder(quantity)
+    case quantity
+    when 1
+      "#{quantity} bottle of beer"
+    else
+      "#{quantity} bottles of beer"
+    end
+  end
+
+  def no_more_phrase_builder
+    'no more bottles of beer'
+  end
+
+  def song_line_builder(quantity)
+    if quantity >= 2
+      "#{bottle_phrase_builder(quantity)} on the wall. #{bottle_phrase_builder(quantity)}. Take one down, pass it around, #{bottle_phrase_builder(quantity - 1)} on the wall."
+    elsif quantity == 1
+      "#{bottle_phrase_builder(quantity)} on the wall. #{bottle_phrase_builder(quantity)}. Take one down, pass it around, #{no_more_phrase_builder} on the wall."
+    end
   end
 end
--- spec/some_class_spec.rb
+++ spec/some_class_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe BottlesSongMaker do
   FIRST_LINE = '99 bottles of beer on the wall. 99 bottles of beer. Take one down, pass it around, 98 bottles of beer on the wall.'
+
   LAST_LINE = 'No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.'
+
+  TWO_BOTTLES_LEFT = '2 bottles of beer on the wall. 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall.'
+
+  ONE_BOTTLE_LEFT = '1 bottle of beer on the wall. 1 bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.'
 
   before(:each) do
     @song = BottlesSongMaker.new
@@ -17,6 +22,14 @@ describe BottlesSongMaker do
     it 'should have a correct first and last phrase' do
       expect(@lyric_sheet.first).to eq(FIRST_LINE)
       expect(@lyric_sheet.last).to eq(LAST_LINE)
+    end
+
+    it 'should have the correct phrase when there are two bottles left' do
+      expect(@lyric_sheet[@lyric_sheet.length - 3]).to eq(TWO_BOTTLES_LEFT)
+    end
+
+    it 'should have the correct phrase when there is one bottle left' do
+      expect(@lyric_sheet[@lyric_sheet.length - 2]).to eq(ONE_BOTTLE_LEFT)
     end
   end
 end
